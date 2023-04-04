51be2663bc74325a56a49918163580ce406d7673
Tedd OKANO <okano.akifumi@gmail.com>
2023-04-04 12:45:16 +0900

    "docs/" update

--- docs/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tempsensor_0',['TempSensor',['../class_temp_sensor.html',1,'']]]
-];
---
+++ docs/search/variables_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['reg_5firef_0',['reg_IREF',['../class_p_c_a995x.html#a46c5246d562d6611cded19cd126dbcad',1,'PCA995x']]],
+  ['reg_5firefall_1',['reg_IREFALL',['../class_p_c_a995x.html#a57421882293e187f69c27eaa220cb14c',1,'PCA995x']]],
+  ['reg_5fpwm_2',['reg_PWM',['../class_l_e_d_driver.html#a6d32dcec6914f6d7d3b46e182eed52c2',1,'LEDDriver']]]
+];
