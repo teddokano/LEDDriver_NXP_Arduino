fd6eeb801aa4e4148700c389f7faeeecb7ded4d4
Tedd OKANO <okano.akifumi@gmail.com>
2023-05-25 08:35:53 +0900

    docs update for release 0.5.1

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['control_0',['control',['../class_gradation_control.html#aaa9f0a75203764f128858991301bf099',1,'GradationControl::control()'],['../class_gradation_control.html#a53f213556dd557356caffd7400e3fbeb',1,'GradationControl::control(int ctrl)']]],
+  ['control_0',['control',['../class_gradation_control.html#aaa9f0a75203764f128858991301bf099',1,'GradationControl::control'],['../class_gradation_control.html#a53f213556dd557356caffd7400e3fbeb',1,'GradationControl::control(int ctrl)']]],
   ['cycle_1',['cycle',['../class_gradation_control.html#a8cbbebc831b5b3d93fa281dfa87fcd78',1,'GradationControl']]]
 ];
